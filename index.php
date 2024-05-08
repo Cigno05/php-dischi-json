9739dfa025489fd3571d58c57a7cff1757bfe6c6
Cigno05 <paolocalcagni.82@gmail.com>
2024-05-08 19:01:10 +0200

    ADD style.css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://kit.fontawesome.com/43ebce74f3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
     <title>Dischi Json</title>
 </head>
 
@@ -54,30 +55,5 @@
     <script src="./js/app.js"></script>
 </body>
 
-<style>
-    .navbar{
-        background-color: #06131F;
-        color: #18D860;
-        .logo {
-            font-size: 40px;
-        }
-
-    }
-
-    main {
-        background-color: #1D2D3C;
-    }
-
-    .card {
-        background-color: #112030;
-        color: white;
-        height: 100%;
-
-        h6 {
-            font-weight: 200;
-        }
-        
-    }
-</style>
 
 </html>
